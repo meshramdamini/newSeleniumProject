d0fad9f26ab06e1bca8e3d525929b2f54e5fc902
meshramdamini <daminmeshram@gmail.com>
2024-04-29 08:56:34 +0530

    page object model with page page factory commit

--- src/main/java/SeleniumNew/NewProjectWithSeleniumGit/App.java
+++
@@ -1,7 +0,0 @@
-package SeleniumNew.NewProjectWithSeleniumGit;
-
-public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
---
+++ src/test/java/PageObjectModel/loginPage.java
@@ -0,0 +1,48 @@
+package PageObjectModel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class loginPage 
+{
+	//sbse pehele webdriver ka object create krte hai q k wbelements ko find krne k liye webdriver ka object chahiy hoga
+	WebDriver driver;
+	
+	//then wbdriver k object ko initioalized krna hai to uske liye hme cunstructor ka use krna hoga
+	//cunstructor ek parameter lega
+	loginPage(WebDriver d)
+	{
+		driver = d;
+		
+		PageFactory.initElements(driver, this);
+	}
+	
+	//then idenfify the web elements
+	
+	@FindBy(id="user-name")
+	WebElement usernam;
+	
+	@FindBy(id="password")
+	WebElement password;
+	
+	@FindBy(id="login-button")
+	WebElement loginBtn;
+	
+	
+	//webelement pe operation perform krne k liye mehod create krni hogi
+	public void enterUsername(String uname)
+	{
+		usernam.sendKeys(uname);
+	}
+	public void enterPassword(String pass)
+	{
+		password.sendKeys(pass);
+	}
+	public void login()
+	{
+		loginBtn.click();
+	}
+
+}
